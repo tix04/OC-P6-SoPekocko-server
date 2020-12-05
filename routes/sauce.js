2915426d83f5bd7b4e8b7650558f6158d9bab671
tix04 <04tiananet@gmail.com>
2020-12-05 18:24:59 +0300

    Created Authentication middleware

--- routes/sauce.js
+++ routes/sauce.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+const auth = require('../middleware/auth');
+
 const sauceCtrl = require('../controllers/sauce');
 
-router.get('/', sauceCtrl.getAllSauces);
-router.post('/', sauceCtrl.createNewSauce);
-router.get('/:id', sauceCtrl.getOneSauce);
-router.put('/:id', sauceCtrl.updateSauce);
-router.delete('/:id', sauceCtrl.deleteSauce);
+router.get('/', auth, sauceCtrl.getAllSauces);
+router.post('/', auth, sauceCtrl.createNewSauce);
+router.get('/:id', auth, sauceCtrl.getOneSauce);
+router.put('/:id', auth, sauceCtrl.updateSauce);
+router.delete('/:id', auth, sauceCtrl.deleteSauce);
 
 module.exports = router;
