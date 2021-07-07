5cc839f2db95c79c79a45107bf363df3f52ba609
tix04 <04tiananet@gmail.com>
2021-07-07 12:02:15 +0300

    fix: included images folder

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,7 @@ module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
     const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET'); 
-   
+    
     const userId = decodedToken.userId;
     if (req.body.userId && req.body.userId !== userId) {
       throw 'Invalid user ID';
@@ -12,9 +12,9 @@ module.exports = (req, res, next) => {
       console.log('succeeded auth test');
       next();
     }
-  } catch {
+  } catch(err) {
     res.status(401).json({
-      error: new Error('Invalid request!')
+      error: err
     });
   }
 };
